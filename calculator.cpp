26d6033b9f923435438220613f1b98e5c2403e58
Sparix <54955798+Sparix@users.noreply.github.com>
2021-12-01 12:16:30 +0200

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,15 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+<<<<<<< HEAD
+	return a + b + 0.5;
+=======
+	return a + b;
+>>>>>>> cbcb06d... formatting: use tabs instead of spaces
+}
+
+int Calculator::Sub (double a, double b)
+{
+	return Add (a, -b);
+}
